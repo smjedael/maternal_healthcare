4d73a623f1b81d994458242e7d40ad2381250d80
unknown <smjedael@uncc.edu>
2018-09-19 12:57:59 -0400

    pushing revised web and app.py files

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 import pandas as pd
 import numpy as np
 
+from datetime import datetime
+
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -27,11 +32,29 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-pregnancy_related_mortality = Base.classes.pregnancy_related_mortality
-maternity_summary = Base.classes.maternity_summary
-maternal_mortality_summary = Base.classes.maternal_mortality_summary
-maternity_random_sample = Base.classes.maternity_random_sample
-us_female_census_summary = Base.classes.us_female_census_summary
+tbl_prm = Base.classes.pregnancy_related_mortality
+tbl_ms = Base.classes.maternity_summary
+tbl_mms = Base.classes.maternal_mortality_summary
+tbl_mrs = Base.classes.maternity_random_sample
+tbl_ufcs = Base.classes.us_female_census_summary
+
+# Create dataframes for each table
+stmt = db.session.query(tbl_prm).statement
+df_prm = pd.read_sql_query(stmt, db.session.bind)
+
+stmt = db.session.query(tbl_ms).statement
+df_ms = pd.read_sql_query(stmt, db.session.bind)
+
+stmt = db.session.query(tbl_mms).statement
+df_mms = pd.read_sql_query(stmt, db.session.bind)
+
+stmt = db.session.query(tbl_mrs).statement
+df_mrs = pd.read_sql_query(stmt, db.session.bind)
+
+stmt = db.session.query(tbl_ufcs).statement
+df_ufcs = pd.read_sql_query(stmt, db.session.bind)
+
+
 
 @app.route("/")
 def index():
@@ -39,64 +62,113 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+@app.route("/plt-matmorbidity/")
+def plt_matmorbidity_page():
+    """Return the Plot - Maternal Morbidity page."""
+    return render_template("plt-matmorbidity.html")
 
 
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
-
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
+@app.route("/plt-matmortality/")
+def plt_matmortality_page():
+    """Return the Plot - Maternal Mortality page."""
+    return render_template("plt-matmortality.html")
 
 
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+@app.route("/comp-matmorbidity/")
+def comp_matmorbidity_page():
+    """Return the Comparison - Maternal Morbidity page."""
+    return render_template("comp-matmorbidity.html")
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+
+@app.route("/comp-matmortality/")
+def comp_matmortality_page():
+    """Return the Comparison - Maternal Mortality page."""
+    return render_template("comp-matmortality.html")
+
+
+@app.route("/regression/")
+def regression_page():
+    """Return the Regression Analysis page."""
+    return render_template("regression.html")
+
+
+@app.route("/data/")
+def data_page():
+    """Return the Data page."""
+    return render_template("data.html")
+
+
+@app.route("/regression/<dataframe>/<predictors>/<response>")
+# Need to look into being able to filter the table
+def perform_regression(dataframe, predictors, response):
+    # Perform Multivariate Regression
+    scale = StandardScaler()
+    df = dataframe
+
+    # Here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression,
+    #then use X = df['Interest_Rate'] for example. Alternatively, you may add additional variables within the brackets.
+
+    column_list = predictors.split('&')
+
+    X = df[column_list]
+    Y = df[response]
+
+    X[column_list] = scale.fit_transform(X[column_list].as_matrix())
+
+    est = sm.OLS(Y, X).fit()
+
+    regression_results = {}
+    regression_results['Dep. Variable'] = response
+    regression_results['Model'] = 'OLS'
+    regression_results['Method'] = 'Least Squares'
+    regression_results['Date'] = datetime.now().strftime('%a, %d %b %Y')
+    regression_results['Time'] = datetime.now().strftime('%H:%M:%S')
+    regression_results['No. Observations'] = np.around(est.nobs, 0)
+    regression_results['Df Residuals'] = np.around(est.df_resid, 0)
+    regression_results['Df Model'] = est.df_model
+    regression_results['Covariance Type'] = est.cov_type
+    regression_results['R-Squared'] = np.around(est.rsquared, 4)
+    regression_results['Adj. R-Squared'] = np.around(est.rsquared_adj, 4)
+    regression_results['F-statistic'] = np.around(est.fvalue, 4)
+    regression_results['Prob (F-statistic)'] = np.around(est.f_pvalue, 4)
+    regression_results['Log-Likelihood'] = np.around(est.llf, 4)
+    regression_results['AIC'] = np.around(est.aic, 4)
+    regression_results['BIC'] = np.around(est.bic, 4)
+
+    params = []
+    coef = []
+    std_err = []
+    t_values = []
+    p_values = []
+    low_conf = []
+    upp_conf = []
+    conf_int = est.conf_int().T
+
+    for i in range(len(est.params)):
+        params.append(est.params.index[i])
+        coef.append(np.around(est.params[i], 4))
+        std_err.append(np.around(est.bse[i], 4))
+        t_values.append(np.around(est.tvalues[i], 4))
+        p_values.append(np.around(est.pvalues[i], 4))
+        low_conf.append(np.around(conf_int.iloc[0, i], 4))
+        upp_conf.append(np.around(conf_int.iloc[1, i], 4))
+
+    regression_results['Parameters'] = params
+    regression_results['Coefficient'] = coef
+    regression_results['Standard Error'] = std_err
+    regression_results['T-Values'] = t_values
+    regression_results['P-Values'] = p_values
+    regression_results['Lower Confidence'] = low_conf
+    regression_results['Upper Confidence'] = upp_conf
+
+    return jsonify(regression_results)
+
+
+@app.route("/plots/scatter/<dataframe>/<xValue>/<yValue>/<xAxisLabel>/<yAxisLabel>")
+def plt_scatter(dataframe, xValue, yValue, xAxisLabel, yAxisLabel):
+    df = dataframe
+    results = {}
+    return jsonify(results)
 
 
 if __name__ == "__main__":
